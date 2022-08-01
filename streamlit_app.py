771ebbb5e410f8d922233f25f5a937993caaa941
fedynyshynivannasnow <110411161+fedynyshynivannasnow@users.noreply.github.com>
2022-08-01 19:13:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Helthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omega')
-streamlit.text('Kale')
-streamlit.text('Egg')
+streamlit.text('Omega 3')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard Boiled Egg')

85dcf0b1bbccf4acd0dbd111af16e2cfc93007ae
fedynyshynivannasnow <110411161+fedynyshynivannasnow@users.noreply.github.com>
2022-08-01 19:27:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('🍏 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥦 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Avocado Toast')
+
+streamlit.header('🥑 Build Your Own Fruit Smuthie 🍏')
